31d29733d13858903f90dfd315683540c4e0f8ed
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-06-16 22:33:30 +0530

    Sharing the common dependencies using the share module in the webpack module ad also creating the bootstrap files indivdually for the standalone running child applications.

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');   //this plugin simplifies the creation of HTML files to serve your webpack bundles.
+const ModuleFedarationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+// this plugin allows us to share code between different webpack builds, so that we can create micro-frontends or share components between different applications.
 
 module.exports = {
     mode: 'development',
@@ -6,6 +8,14 @@ module.exports = {
         port: 8081,
     },
     plugins: [
+        new ModuleFedarationPlugin({
+            name: 'products',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProductsIndex': './src/index', // this is the file that we want to expose to the world, so that other applications can use it.
+            },
+            shared: ['faker']
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
             // so this plugin is gonna take a look at whatever files are coming out of our webpack process, it's going to find those 
