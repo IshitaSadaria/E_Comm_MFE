0175a412868bba9156428d33e8d79e1787827ba9
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-06-21 23:57:41 +0530

    method creation for mounting of the local index.html files to the container application

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,12 +1,43 @@
 import faker from "faker";
 
-let products = '';
+const mount = (el) => {
+
+    let products = '';
 
 for ( let i=0; i<3; i++) {
     const name = faker.commerce.productName();
     products += `<div>${name}</div>`;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+el  .innerHTML = products;
 // This code generates 3 random product names using Faker.js and displays them in the HTML element with id 'dev-products'.
 
+}
+
+
+//context / situation 1:
+//we are running this file in development  in insolation, 
+//we are using local index.html file,
+// which definitely has eleement with id dev-products
+//we want to immediately render our app into that element
+if( process.env.NODE_ENV === 'development'){
+    const el = document.querySelector('#dev-products');
+
+    //assuming our container app does have an element with id dev-products
+    if (el) {
+        // we are running this file in isolation
+        mount(el);
+    }
+}
+
+
+
+
+//context / situation 2:
+//we are running this file in development or production, through container app,
+// No gurantee that an element with id dev-products exists
+//we do not want immediately render our app 
+
+
+
+export { mount };
